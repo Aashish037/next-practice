3cd771c1ef1538309eeece8d715b81745b2b891d
Asish Singh <Aashishsingh1317@gmail.com>
2025-02-07 00:47:24 +0530

    Third

--- app/components/hello.tsx
+++ app/components/hello.tsx
@@ -1,5 +1,13 @@
 "use client";
 
 function Hello(){
+    console.log("Hello!, I am a client");
 
+    return (
+        <div>
+            <h1>Client</h1>
+        </div>
+    )
 }
+
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 // import Image from "next/image";
+import Hello from './components/hello'
 
 export default function Home() {
   return (
-    <div> HEllo </div>
+    <>
+      <div> HEllo !!! whats up </div>
+      <Hello />
+    </>
+    
   );
 }
